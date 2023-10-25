89a86f4e0ffd4b7d8b4ed05d5db7c7976b744e52
crazyhaller <suvigyamadrid@gmail.com>
2023-10-25 19:37:20 +0530

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
 const contactRoute = require('./routes/contactRoute')
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const port = process.env.PORT || 5000
 connectDB()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
